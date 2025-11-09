28bfb4f694f736e4cdf162e815c643a5695f901c
waldon <waldonjulienjr@gmail.com>
2025-11-09 16:03:26 -0600

    gave some structure to create recipies and profile page and my and favorite recipes

---
+++ src/profilePage/CreateRecipe.jsx
@@ -0,0 +1,10 @@
+
+
+export default CreateRecipe;
+
+const CreateRecipe = () => {
+return
+
+// This is going to be a form
+
+}
---
+++ src/profilePage/MyRecipes.jsx
@@ -0,0 +1,4 @@
+/* 
+I want to have personally made recipes, saved recipies and favorite recipies. 
+
+*/
