ae64aa3bf901135f0c22b0dd927b93e96b47fd39
Rudy Acosta <rudyacosta323@gmail.com>
2025-11-16 21:27:18 -0600

    Mounted API base routes

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ import express from "express";
 const app = express();
 export default app;
 
-import getUserFromToken from "#middleware/getUserFromToken";
-import ordersRouter from "#api/orders";
-import productsRouter from "#api/products";
-import usersRouter from "#api/users";
+import getUserFromToken from "./src-backend/middleware/getUserFromToken.js";
+import recipesRouter from "./src-backend/api/recipesAPI.js";
+import profileRouter from "./src-backend/api/profileAPI.js";
+import topTenRouter from "./src-backend/api/topTenAPI.js";
+import ingredientsRouter from "./src-backend/api/ingredientsAPI.js";
+import usersRouter from "./src-backend/api/users.js";
 
 // Express body-parsing middleware. It tells Express to automatically read JSON data from the request body and turn it into a JavaScript object on req.body
 app.use(express.json());
@@ -14,8 +16,10 @@ app.use(express.json());
 app.use(getUserFromToken);
 
 // Each one connects to a url path
-app.use("/products", productsRouter);
-app.use("/orders", ordersRouter);
+app.use("/recipes", recipesRouter);
+app.use("/profile", profileRouter);
+app.use("/top-ten", topTenRouter);
+app.use("/ingredients", ingredientsRouter);
 app.use("/users", usersRouter);
 
 // catches all errors that are not caught by error handlers in the router
