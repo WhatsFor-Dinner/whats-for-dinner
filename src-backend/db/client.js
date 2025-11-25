4701d55dffa1f8fbafaa83c58f1ab9b8e84e5704
Rudy Acosta <rudyacosta323@gmail.com>
2025-11-24 19:10:47 -0600

    fixed connection issue

--- src-backend/db/client.js
+++ src-backend/db/client.js
@@ -2,7 +2,7 @@ import pg from "pg";
 
 // connects to the postgresSQL DB
 const db = new pg.Client(process.env.DATABASE_URL);
-
+await db.connect();
 // exports it so it can run queries in other files
 export default db;
 
